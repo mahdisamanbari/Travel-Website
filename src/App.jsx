c4980a0eadf18fee2970780ceed683a1edd0ebdc
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-11-12 08:32:33 +0330

    topbar component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import TopBar from './components/TopBar'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="">
+      <TopBar/>
+    </div>
   )
 }
 

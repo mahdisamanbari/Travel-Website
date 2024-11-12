0d07d7865b69ecb1aff3a088770523359688a726
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-11-12 13:50:47 +0330

    footer completed and travel website is done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { BsChatSquareDots } from 'react-icons/bs';
 
 const Footer = () => {
   return (
-    <div>
+    <div className='max-w-[1140px] w-full py-8 m-auto border-t-4 '>
+        <div className="flex items-center justify-center m-auto">
+            <BsChatSquareDots size={30} className='text-[var(--primary-dark)]'/>
+            <h1 className="text-xl font-bold text-gray-700 p-2 ">WEEKAWAY</h1>
+        </div>
+        <h3 className="text-center m-4 text-gray-700">built whit ❤️ by Mahdi Samanbari</h3>
       
     </div>
   )

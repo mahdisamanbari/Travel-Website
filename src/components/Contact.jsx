aa77c9600680d9ac9d97870694b32c3263942857
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-11-12 13:07:29 +0330

    contact component created

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id='#contact'>
+        
+            <h2 className="">Send us a message</h2>
+            <p className="">We Are Standing By!</p>
+        
+    </div>
+  )
+}
+
+export default Contact

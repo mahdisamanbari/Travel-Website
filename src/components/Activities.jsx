b8ec85173ae1e1e3ef16ab2759924b192c81834f
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-11-12 10:06:31 +0330

    activities component completed

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Activities = () => {
   return (
-    <div>
+    <div className='max-w-[1140px] m-auto w-full md:flex mt-[-75px]'>
+        <div className="relative p-4">
+            <h3 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white font-bold text-2xl">Resorts</h3>
+            <img src="https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+             alt="" 
+             className="border-4 border-white shadow-lg rounded-sm" />
+        </div>
+        <div className="relative p-4">
+            <h3 className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white font-bold text-2xl">Resorts</h3>
+            <img src="https://images.unsplash.com/photo-1599640842225-85d111c60e6b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+             alt="" 
+             className="border-4 border-white shadow-lg rounded-sm" />
+        </div>
+        <div className="relative p-4">
+            <h3 className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white font-bold text-2xl">Resorts</h3>
+            <img src="https://images.unsplash.com/photo-1544551763-46a013bb70d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+             alt="" 
+             className="border-4 border-white shadow-lg rounded-sm" />
+        </div>
       
     </div>
   )
